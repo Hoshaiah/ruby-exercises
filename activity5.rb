2f83f8754bd2d1c53bb8b41c72049ab2777f9678
hoshaiah <domingohoshaiah@gmail.com>
2022-01-19 20:30:01 +0800

    added functional version of code

--- activity5.rb
+++ activity5.rb
@@ -1,4 +1,7 @@
-def find_smallest_integer(array)
+array1 = [34,15,88,2]
+array2 = [34,-345,-1,100] 
+
+def find_smallest_integer_imperative(array)
     smallest = array[0]
     (1..array.length-1).each do |index|
         if array[index] < smallest
@@ -9,5 +12,18 @@ def find_smallest_integer(array)
     return smallest
 end
 
-puts find_smallest_integer([34,15,88,2])
-puts find_smallest_integer([34,-345,-1,100])
+
+def find_smallest_integer_functional(array)
+
+    smallest = array.reduce(array[0]){|answer,num| num < answer ? num : answer }
+
+    return smallest 
+end
+
+puts "imperative"
+puts find_smallest_integer_imperative(array1)
+puts find_smallest_integer_imperative(array2)
+puts "functional"
+puts find_smallest_integer_functional(array1)
+puts find_smallest_integer_functional(array2)
+
