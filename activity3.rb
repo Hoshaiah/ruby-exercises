20a78b1ef3ed32b5c5d248f471f9557b84cf5f51
hoshaiah <domingohoshaiah@gmail.com>
2022-01-19 17:11:22 +0800

    added activity4 and minor changes on activity3

--- activity3.rb
+++ activity3.rb
@@ -1,4 +1,7 @@
 class Card
+    attr_reader( :number, :suit)
+    attr_writer( :number, :suit)
+    attr_accessor(:suit)
     def initialize(number, suit)
         @number = number
         @suit = suit
@@ -15,6 +18,5 @@ card1 = Card.new("J","hearts")
 card2 = Card.new("A","diamonds")
 card3 = Card.new("3","clubs")
 
-card1.show_card
-card2.show_card
-card3.show_card
+# card2.show_card
+# card3.show_card
